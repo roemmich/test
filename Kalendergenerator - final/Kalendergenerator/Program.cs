07fa296107698847910d4e324915e3045005d023
roemmich <roemmich.23@gmail.com>
2018-11-27 21:49:15 +0100

    kalendergenerator-final

---
+++ Kalendergenerator - final/Kalendergenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kalendergenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            YearPrinter printer = new YearPrinter();
+            string firstArgument = "";
+            string secondArgument = "";
+            try
+            {
+                firstArgument = args[0];
+                secondArgument = args[1];
+            }
+            catch (System.IndexOutOfRangeException)
+            { }
+            CommandLineParser clParser = new CommandLineParser(firstArgument, secondArgument);
+            // Konsolenausgabe des Jahres.
+            printer.PrintYear(new Year(clParser.Year, clParser.WeekStartday));
+        }
+    }
+}
---
+++ Kalendergenerator - final/Kalendergenerator/UnvalidYearException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kalendergenerator
+{
+    public class UnvalidYearException : Exception
+    {
+        public UnvalidYearException()
+        {
+        }
+
+        public UnvalidYearException(string message)
+            : base("Ungültiges Jahr eingegeben! Hinweis: Das Jahr muss vierstellig sein.")
+        {
+        }
+
+        public UnvalidYearException(string message, Exception inner)
+            : base("Ungültiges Jahr eingegeben! Hinweis: Das Jahr muss vierstellig sein.", inner)
+        {
+        }
+    }
+}
