90ddc117e0752b61207ae406bda315a9ede40966
roemmich.23 <roemmich.23@gmail.com>
2018-12-13 09:53:51 +0100

    astronauts

---
+++ AstronautsInSpace/AstronautsInSpace/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+namespace AstronautsInSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient w = new WebClient())
+            {
+                var json = w.DownloadString("http://api.open-notify.org/astros.json");
+                dynamic stuff = JObject.Parse(json);
+                foreach (var item in stuff.people)
+                    Console.WriteLine("{0} {1}", item.name, item.craft);
+                Console.WriteLine("Anzahl: " + stuff.number);
+            }
+        }
+    }
+}
